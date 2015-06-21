a5068719fee3c77c2e6509f16c221de6cd7570c4
17bhuey <17bhuey@pinewood.edu>
2015-06-20 17:07:26 -0700

    Delete euler1.py

--- euler1.py
+++
@@ -1,9 +0,0 @@
-def problem1():
-	sum = 0
-	print "This will find the sum of all multiples of 3 and 5 less than 1000"
-	for i in range(1000):
-		if i % 3 == 0 or i % 5 == 0:
-			sum += i
-	print sum
-
-problem1()
